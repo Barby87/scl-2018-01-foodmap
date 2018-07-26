0118542ff23ab7b9abcda0b601eb133a7f524b3f
Barby87 <bcaceres.sepulveda@hotmail.com>
2018-07-25 21:38:45 -0300

    Se agregó marcador a ubicación actual del usuario

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,32 @@
 function initMap() {
   const ubicacion = new localization(()=>{
 
+    const myLatLng = {lat: ubicacion.latitude, lng: ubicacion.longitude};
+
+    var text = '<h6>Mi posición actual</h6>'
+
     const options = {
       // Centrando mapa en la posición actual del usuario
-      center: {
-        lat: ubicacion.latitude, 
-        lng: ubicacion.longitude
-      },
-
+      center: myLatLng,
       zoom: 15
     }
 
     // Dibujando mapa dentro del div "map"
     var map = document.getElementById('map');
     const mapa = new google.maps.Map(map, options);
-     
+    const marcador = new google.maps.Marker({
+      position: myLatLng,
+      map: mapa,
+      title: "Mi primer marcador"
     });
 
+    var informacion = new google.maps.InfoWindow({
+      content: text
+    });
+
+    marcador.addListener('click', function(){
+      informacion.open(mapa, marcador);
+    });
+
+  });
 }
